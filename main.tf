a03ece7b3305c500f808abe9c6199df563dbddf5
Praveen Ambati <praveen.ambati@nttdata.com>
2026-06-26 08:40:27 +0530

    remove vm

--- main.tf
+++ main.tf
@@ -12,43 +12,43 @@ module "virtual_network" {
   version = "0.8.1"
 
   resource_group_name = local.resource_group_name
-  location = var.location
+  location            = var.location
   name                = local.resource_names.virtual_network_name
   subnets             = var.virtual_network_subnets
   address_space       = var.virtual_network_address_space
   tags                = var.tags
 }
 
-module "virtual_machine" {
-  source  = "Azure/avm-res-compute-virtualmachine/azurerm"
-  version = "0.18.1"
+# module "virtual_machine" {
+#   source  = "Azure/avm-res-compute-virtualmachine/azurerm"
+#   version = "0.18.1"
 
-  resource_group_name        = local.resource_group_name
-  os_type                    = "linux"
-  name                       = local.resource_names.virtual_machine_name
-  sku_size                   = var.virtual_machine_sku
-  location                   = var.location
-  zone                       = "1"
-  encryption_at_host_enabled = false
+#   resource_group_name        = local.resource_group_name
+#   os_type                    = "linux"
+#   name                       = local.resource_names.virtual_machine_name
+#   sku_size                   = var.virtual_machine_sku
+#   location                   = var.location
+#   zone                       = "1"
+#   encryption_at_host_enabled = false
 
-  source_image_reference = {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
+#   source_image_reference = {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts-gen2"
+#     version   = "latest"
+#   }
 
-  network_interfaces = {
-    private = {
-      name = local.resource_names.network_interface_name
-      ip_configurations = {
-        private = {
-          name                          = local.resource_names.network_interface_name
-          private_ip_subnet_resource_id = module.virtual_network.subnets["example"].resource_id
-        }
-      }
-    }
-  }
+#   network_interfaces = {
+#     private = {
+#       name = local.resource_names.network_interface_name
+#       ip_configurations = {
+#         private = {
+#           name                          = local.resource_names.network_interface_name
+#           private_ip_subnet_resource_id = module.virtual_network.subnets["example"].resource_id
+#         }
+#       }
+#     }
+#   }
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
